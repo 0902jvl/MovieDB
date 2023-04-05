938efcf17e1ad0d1369200d100ffde029d975464
John <finvvs@ukr.net>
2023-04-05 13:35:52 +0300

    modified script.js Automate questions with a loop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+"use strict";
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovidDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,10 +9,26 @@ const personalMovidDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько его оцените?", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько его оцените?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", "");
+    const b = prompt("На сколько его оцените?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("OK");
+    } else {
+        i--;
+        console.log("ereor");
+    }
+}
 
-personalMovidDB.movies[a] = b;
-personalMovidDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
