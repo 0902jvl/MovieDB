2efa0f91a0e35b4aecbb3bec3705bdffd10efc6e
John <finvvs@ukr.net>
2023-04-05 12:19:48 +0300

    modifile script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovidDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", "");
+const b = prompt("На сколько его оцените?", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("На сколько его оцените?", "");
+
+personalMovidDB.movies[a] = b;
+personalMovidDB.movies[c] = d;
